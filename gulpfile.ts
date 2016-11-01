1ef090a7acfd93c6209a32e926b19f9d1e796efc
Denis Loginov <dloginov@mit.edu>
2016-11-01 19:43:44 +0000

    Add reporting for Jasmine tests run through Gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,7 @@
+const argv = require('yargs').argv;
 const gulp = require('gulp');
 const jasmineTest = require('gulp-jasmine');
+const reporters = require('jasmine-reporters');
 
 const paths = {
 	src: ['src/*.ts'],
@@ -13,7 +15,12 @@ gulp.task('watch', () => {
 gulp.task('test', () => {
   return gulp
     .src(paths.specs)
-    .pipe(jasmineTest());
+    .pipe(jasmineTest({
+      reporter: new reporters.TerminalReporter({
+        verbosity: argv.verbose ? 3 : 2,
+        color: true,
+      }),
+    }));
 });
 
 gulp.task('default', ['watch', 'test']);
