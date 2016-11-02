f98006a53b82f86beca5bfcbfa2139b00249b3a2
Denis Loginov <dloginov@mit.edu>
2016-11-02 00:55:36 +0000

    Generate Istanbul coverage reports through Gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,18 +1,53 @@
 const argv = require('yargs').argv;
 const gulp = require('gulp');
+const istanbul = require('gulp-istanbul');
 const jasmineTest = require('gulp-jasmine');
+const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
 const reporters = require('jasmine-reporters');
+const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
+
+const tsProject = ts.createProject('tsconfig.json');
 
 const paths = {
-	src: ['src/*.ts'],
+	src: ['src/!(*.spec).ts'],
 	specs: ['src/*.spec.ts'],
+	reports: 'reports',
+	coverage: () => paths.reports + '/coverage',
 };
 
 gulp.task('watch', () => {
   gulp.watch(paths.src, ['test']);
+  gulp.watch(paths.specs, ['test']);
 });
 
-gulp.task('test', () => {
+const remapCoverageFiles = () => {
+  const reports: { [key: string]: string } = {
+    'html': paths.coverage(),
+  };
+  if (argv.verbose) {
+    reports['text-summary'] = null;
+  }
+  return gulp
+    .src(paths.coverage() + '/coverage-final.json')
+    .pipe(remapIstanbul({
+      reports: reports,
+    }));
+};
+
+gulp.task('coverage', () => {
+    return tsProject.src()
+      .pipe(sourcemaps.init())
+      .pipe(tsProject()).js
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(paths.coverage()))
+      .pipe(istanbul({
+        includeUntested: true,
+      }))
+      .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['coverage'], () => {
   return gulp
     .src(paths.specs)
     .pipe(jasmineTest({
@@ -20,7 +55,11 @@ gulp.task('test', () => {
         verbosity: argv.verbose ? 3 : 2,
         color: true,
       }),
-    }));
+    }))
+    .pipe(istanbul.writeReports({
+      dir: paths.coverage(),
+      reporters: ['json'],
+    })).on('end', remapCoverageFiles);
 });
 
 gulp.task('default', ['watch', 'test']);
