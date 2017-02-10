828145d4da1cccb1e85b3606c8dcbc345ac7516a
Denis Loginov <dloginov@mit.edu>
2017-02-10 06:47:01 +0000

    Use https.request instead of request-promise
    This enhancement removes dependency on heavy 'request-promise'
    package in favor of native 'https.request'.

--- src/cloudformation.spec.ts
+++ src/cloudformation.spec.ts
@@ -1,4 +1,4 @@
-import * as requestPromise from 'request-promise';
+import * as https from 'https';
 
 import { Request, Response, sendResponse } from './cloudformation';
 import { testError} from './fixtures/support';
@@ -7,7 +7,8 @@ import { Callback, Dict } from './types';
 
 describe('sendResponse()', () => {
   const fakeRequestType = 'Update';
-  const fakeResponseUri = 'https://pre-signed-S3-url-for-fake-response';
+  const fakeResponseHostname = 'fake-response-endpoint.s3.amazonaws.com';
+  const fakeResponsePath = '/fake/path';
   const fakeStackId = 'fake-stack';
   const fakeRequestId = 'fake-request-abcd-1234';
   const fakeResponseType = 'fake-response-type';
@@ -18,8 +19,10 @@ describe('sendResponse()', () => {
 
   let fakeEvent: Request & Response;
   let fakeResponseData: Dict<any>;
+  let fakeResponse: (responseId: string) => string;
 
-  let spyOnRequestPromisePut: jasmine.Spy;
+  let spyOnHttpsRequest: jasmine.Spy;
+  let spyOnHttpsRequestConstructor: jasmine.Spy;
 
   beforeEach(() => {
     fakeResponseData = {
@@ -27,26 +30,54 @@ describe('sendResponse()', () => {
     };
     fakeEvent = {
       RequestType: fakeRequestType,
-      ResponseURL: fakeResponseUri,
+      ResponseURL: 'https://' + fakeResponseHostname + fakeResponsePath,
       StackId: fakeStackId,
       RequestId: fakeRequestId,
       ResourceType: fakeResponseType,
       LogicalResourceId: fakeLogicalResourceId,
+      PhysicalResourceId: fakePhysicalResourceId,
       ResourceProperties: {},
       OldResourceProperties: {},
       Status: fakeResponseStatus,
       Reason: fakeResponseReason,
       Data: fakeResponseData,
     };
+    fakeResponse = (resourceId: string) => stringify({
+      Status: fakeResponseStatus,
+      Reason: fakeResponseReason,
+      PhysicalResourceId: resourceId,
+      StackId: fakeStackId,
+      RequestId: fakeRequestId,
+      LogicalResourceId: fakeLogicalResourceId,
+      Data: fakeResponseData,
+    });
 
-    spyOnRequestPromisePut= spyOn(requestPromise, 'put')
-      .and.returnValue(Promise.resolve());
+    spyOnHttpsRequest = jasmine.createSpyObj('spyOnHttpsRequest', ['on', 'end']);
+    (spyOnHttpsRequest as any).end
+      .and.callFake((response: string, encoding: string, callback: Callback) => callback());
+    spyOnHttpsRequestConstructor = spyOn(https, 'request')
+      .and.returnValue(spyOnHttpsRequest);
   });
 
   describe('calls', () => {
-    describe('requestPromise.put() once with correct parameters when PhysicalResourceId is', () => {
+    it('https.request() once with correct parameters', (done: Callback) => {
+      const callback = () => {
+        expect(spyOnHttpsRequestConstructor).toHaveBeenCalledWith({
+          hostname: fakeResponseHostname,
+          path: fakeResponsePath,
+          method: 'PUT',
+          headers: {
+            'content-length': fakeResponse(fakePhysicalResourceId).length,
+          }
+        });
+        expect(spyOnHttpsRequestConstructor).toHaveBeenCalledTimes(1);
+        done();
+      };
+      sendResponse(fakeEvent, null, callback);
+    });
+
+    describe('https.request().end() once with correct parameters when PhysicalResourceId is', () => {
       it('defined', (done: Callback) => {
-        fakeEvent.PhysicalResourceId = fakePhysicalResourceId;
         checkPut(fakePhysicalResourceId, done);
       });
       it('undefined', (done: Callback) => {
@@ -55,33 +86,23 @@ describe('sendResponse()', () => {
       });
 
       function checkPut(resourceId: any, done: Callback) {
-        sendResponse(fakeEvent, null, () => {
-          expect(spyOnRequestPromisePut).toHaveBeenCalledWith({
-            uri: fakeResponseUri,
-            body: stringify({
-              Status: fakeResponseStatus,
-              Reason: fakeResponseReason,
-              PhysicalResourceId: resourceId,
-              StackId: fakeStackId,
-              RequestId: fakeRequestId,
-              LogicalResourceId: fakeLogicalResourceId,
-              Data: fakeResponseData,
-            }),
-          });
-          expect(spyOnRequestPromisePut).toHaveBeenCalledTimes(1);
+        const callback = () => {
+          expect((spyOnHttpsRequest as any).end).toHaveBeenCalledWith(
+            fakeResponse(resourceId), 'utf8', callback);
+          expect((spyOnHttpsRequest as any).end).toHaveBeenCalledTimes(1);
           done();
-        });
+        };
+        sendResponse(fakeEvent, null, callback);
       };
     });
 
-    it('callback with an error if requestPromise.put() returns an error', (done: Callback) => {
-      spyOnRequestPromisePut.and.returnValue(Promise.reject('requestPromise.put()'));
-      testError(sendResponse, fakeEvent, done);
+    it('https.request().on() with correct parameters', (done: Callback) => {
+      const callback = () => {
+        expect((spyOnHttpsRequest as any).on).toHaveBeenCalledWith('error', callback);
+        expect((spyOnHttpsRequest as any).on).toHaveBeenCalledTimes(1);
+        done();
+      };
+      sendResponse(fakeEvent, null, callback);
     });
-  });
-
-  it('does not produce an error when called with correct parameters ' +
-     'and requestPromise.put() does not return an error', (done: Callback) => {
-    testError(sendResponse, fakeEvent, done, false);
   });
 });
--- src/cloudformation.ts
+++ src/cloudformation.ts
@@ -1,6 +1,7 @@
-import * as requestPromise from 'request-promise';
-
+import * as https from 'https';
 import * as stringify from 'json-stable-stringify';
+import * as url from 'url';
+
 import { log } from './log';
 import { Callback, Dict } from './types';
 
@@ -23,19 +24,26 @@ export interface Response {
 };
 
 export function sendResponse(event: Request & Response, context: any, callback: Callback) {
-  requestPromise.put({
-    uri: event.ResponseURL,
-    body: stringify({
-      Status: event.Status,
-      Reason: event.Reason,
-      PhysicalResourceId: event.PhysicalResourceId || event.LogicalResourceId,
-      StackId: event.StackId,
-      RequestId: event.RequestId,
-      LogicalResourceId: event.LogicalResourceId,
-      Data: event.Data,
-    }),
-  }).then(() => callback())
-    .catch(callback);
+  const parsedUrl = url.parse(event.ResponseURL);
+  const response = stringify({
+    Status: event.Status,
+    Reason: event.Reason,
+    PhysicalResourceId: event.PhysicalResourceId || event.LogicalResourceId,
+    StackId: event.StackId,
+    RequestId: event.RequestId,
+    LogicalResourceId: event.LogicalResourceId,
+    Data: event.Data,
+  });
+  const request = https.request({
+    hostname: parsedUrl.hostname,
+    path: parsedUrl.path,
+    method: 'PUT',
+    headers: {
+      'content-length': response.length,
+    }
+  });
+  request.on('error', callback);
+  request.end(response, 'utf8', callback);
 };
 
 export function sendOnError(request: Request, err: Error, callback: Callback) {
