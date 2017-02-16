05b6dd7e6bfe62392ce45c5fea0451b4e2dc0e95
Denis Loginov <dloginov@mit.edu>
2017-02-16 02:57:44 +0000

    Implement SAM Functions to manage subnets in the VPC

---
+++ src/vpc.spec.ts
@@ -0,0 +1,178 @@
+import { ec2 } from './aws';
+import { fakeResolve, fakeReject, testArray, testError } from './fixtures/support';
+import { Callback } from './types';
+import { calculateSubnets, createSubnets, describeSubnets, deleteSubnets } from './vpc';
+
+const fakeVpcId = 'fake-vpc-1234';
+const fakeAvailabilityZones = () => ['us-west-2a', 'us-west-2b', 'us-west-2c'];
+const fakeSubnetRanges = () => ['10.0.64.0/20', '10.0.80.0/20', '10.0.96.0/20'];
+const fakeSubnetIds = () => ['fake-subnet-1', 'fake-subnet-2', 'fake-subnet-3'];
+
+describe('calculateSubnets()', () => {
+  let fakeEvent: any;
+
+  beforeEach(() => {
+    fakeEvent = {
+      VpcRange: '10.0.64.0/18',
+      AvailabilityZones: fakeAvailabilityZones(),
+    };
+  });
+
+  it('correctly determines subnet ranges when called with valid parameters', (done: Callback) => {
+    calculateSubnets(fakeEvent, null, (err: Error, subnetRanges: string[]) => {
+      expect(subnetRanges).toEqual(fakeSubnetRanges());
+      done();
+    });
+  });
+
+  describe('calls callback with an error if', () => {
+    afterEach((done: Callback) => {
+      testError(calculateSubnets, fakeEvent, done);
+    });
+
+    it('VpcRange is malformed', () => {
+      fakeEvent.VpcRange = '10.0.0.0/33';
+    });
+    testArray(calculateSubnets, () => fakeEvent, 'AvailabilityZones');
+  });
+
+  it('does not produce an error when called with correct parameters', (done: Callback) => {
+    testError(calculateSubnets, fakeEvent, done, false);
+  });
+});
+
+describe('createSubnets()', () => {
+  let fakeEvent: any;
+
+  let spyOnCreateSubnet: jasmine.Spy;
+
+  beforeEach(() => {
+    fakeEvent = {
+      VpcId: fakeVpcId,
+      AvailabilityZones: fakeAvailabilityZones(),
+      SubnetRanges: fakeSubnetRanges(),
+    };
+
+    spyOnCreateSubnet = spyOn(ec2, 'createSubnet')
+      .and.returnValues.apply(null, fakeSubnetIds().map(subnetId =>
+        fakeResolve({ Subnet: { SubnetId: subnetId }})));
+  });
+
+  describe('calls', () => {
+    it('ec2.createSubnet() with correct parameters', (done: Callback) => {
+      createSubnets(fakeEvent, null, () => {
+        fakeAvailabilityZones().map((zone: string, index: number) => {
+          expect(spyOnCreateSubnet).toHaveBeenCalledWith({
+            VpcId: fakeVpcId,
+            AvailabilityZone: zone,
+            CidrBlock: fakeSubnetRanges()[index],
+          });
+        });
+        expect(spyOnCreateSubnet).toHaveBeenCalledTimes(fakeAvailabilityZones().length);
+        done();
+      });
+    });
+
+    describe('callback with', () => {
+      it('correct response when called with correct parameters', (done: Callback) => {
+        createSubnets(fakeEvent, null, (err: Error, subnetIds: any) => {
+          expect(err).toBeFalsy();
+          expect(subnetIds).toEqual(fakeSubnetIds());
+          done();
+        });
+      });
+
+      describe('an error if', () => {
+        it('ec2.createSubnet() produces an error', (done: Callback) => {
+          spyOnCreateSubnet.and.returnValue(fakeReject('ec2.createSubnet()'));
+          testError(createSubnets, fakeEvent, done);
+        });
+
+        testArray(createSubnets, () => fakeEvent, 'AvailabilityZones');
+        testArray(createSubnets, () => fakeEvent, 'SubnetRanges');
+
+        it('event.AvailabilityZones.length != event.SubnetRanges.length', (done: Callback) => {
+          fakeEvent.SubnetRanges.pop();
+          testError(createSubnets, fakeEvent, done);
+        });
+      });
+    });
+  });
+});
+
+describe('describeSubnets()', () => {
+  let spyOnDescribeSubnets: jasmine.Spy;
+
+  beforeEach(() => {
+    spyOnDescribeSubnets = spyOn(ec2, 'describeSubnets')
+      .and.returnValue(fakeResolve({
+        Subnets: fakeSubnetIds().map(subnetId => ({ SubnetId: subnetId })),
+      }));
+  });
+
+  describe('calls', () => {
+    it('ec2.describeSubnets() with correct parameters', (done: Callback) => {
+      describeSubnets(fakeVpcId, null, () => {
+        expect(spyOnDescribeSubnets).toHaveBeenCalledWith({
+          Filters: [{
+            Name: 'vpc-id',
+            Values: [fakeVpcId],
+          }]
+        });
+        done();
+      });
+    });
+
+    describe('callback with', () => {
+      it('correct response when called with correct parameters', (done: Callback) => {
+        describeSubnets(fakeVpcId, null, (err: Error, subnetIds: any) => {
+          expect(err).toBeFalsy();
+          expect(subnetIds).toEqual(fakeSubnetIds());
+          done();
+        });
+      });
+
+      it('an error if ec2.describeSubnets() produces an error', (done: Callback) => {
+        spyOnDescribeSubnets.and.returnValue(fakeReject('ec2.describeSubnets()'));
+        testError(describeSubnets, fakeVpcId, done);
+      });
+    });
+  });
+});
+
+describe('deleteSubnets()', () => {
+  let spyOnDeleteSubnet: jasmine.Spy;
+
+  beforeEach(() => {
+    spyOnDeleteSubnet = spyOn(ec2, 'deleteSubnet')
+      .and.returnValue(fakeResolve());
+  });
+
+  describe('calls', () => {
+    it('ec2.deleteSubnet() with correct parameters', (done: Callback) => {
+      deleteSubnets(fakeSubnetIds(), null, () => {
+        fakeSubnetIds().map(subnetId => {
+          expect(spyOnDeleteSubnet).toHaveBeenCalledWith({
+            SubnetId: subnetId,
+          });
+        });
+        expect(spyOnDeleteSubnet).toHaveBeenCalledTimes(fakeSubnetIds().length);
+        done();
+      });
+    });
+
+    describe('callback', () => {
+      describe('with an error if', () => {
+        it('ec2.deleteSubnet() produces an error', (done: Callback) => {
+          spyOnDeleteSubnet.and.returnValue(fakeReject('ec2.deleteSubnet()'));
+          testError(deleteSubnets, fakeSubnetIds(), done);
+        });
+        testArray(deleteSubnets, fakeSubnetIds, 'subnetIds');
+      });
+
+      it('without an error when called with correct parameters', (done: Callback) => {
+        testError(deleteSubnets, fakeSubnetIds(), done, false);
+      });
+    });
+  });
+});
---
+++ src/vpc.ts
@@ -0,0 +1,81 @@
+import { Netmask } from 'netmask';
+
+import { ec2 } from './aws';
+import { testEmpty } from './helpers';
+import { Callback } from './types';
+
+export const calculateSubnets = (event: any, context: any, callback: Callback) => {
+  try {
+    testEmpty(event.AvailabilityZones, 'AvailabilityZones');
+
+    const vpc = new Netmask(event.VpcRange);
+    const subnetBits = countBits(event.AvailabilityZones.length);
+
+    let subnet = new Netmask(vpc.base, (vpc.bitmask + subnetBits).toString());
+    callback(null, event.AvailabilityZones.map(() => {
+      const subnetString = subnet.toString();
+      subnet = subnet.next();
+      return subnetString;
+    }));
+  } catch (err) {
+    callback(err);
+  }
+};
+
+const countBits = (n: number) => {
+  let bits = 0;
+  for ( let i = 1; i < n; i <<= 1, bits++ );
+  return bits;
+};
+
+export const createSubnets = (event: { VpcId: string, AvailabilityZones: string[], SubnetRanges: string[] },
+                            context: any, callback: Callback) => {
+  Promise.resolve(event)
+    .then(event => {
+      testEmpty(event.AvailabilityZones, 'AvailabilityZones');
+      testEmpty(event.SubnetRanges, 'SubnetRanges');
+      if (event.AvailabilityZones.length !== event.SubnetRanges.length) {
+        throw Error('Expected AvailabilityZones[] and SubnetRanges[] of equal length');
+      }
+    })
+    .then(() => Promise.all(event.AvailabilityZones
+      .map((zone: string, index: number) =>
+        createSubnet(event.VpcId, zone, event.SubnetRanges[index]))))
+    .then(subnetIds => callback(null, subnetIds))
+    .catch(callback);
+};
+
+const createSubnet = (vpcId: string, zone: string, subnetRange: string) => {
+  return ec2.createSubnet({
+      VpcId: vpcId,
+      AvailabilityZone: zone,
+      CidrBlock: subnetRange,
+    }).promise()
+      .then(data => data.Subnet.SubnetId);
+};
+
+export const describeSubnets = (vpcId: string, context: any, callback: Callback) => {
+  ec2.describeSubnets({
+    Filters: [{
+      Name: 'vpc-id',
+      Values: [vpcId],
+    }],
+  }).promise()
+    .then(data => callback(null, data.Subnets.map(subnet => subnet.SubnetId)))
+    .catch(callback);
+};
+
+export const deleteSubnets = (subnetIds: string[], context: any, callback: Callback) => {
+  Promise.resolve(subnetIds)
+    .then(subnetIds => testEmpty(subnetIds, 'subnetIds'))
+    .then(() => Promise.all(subnetIds
+      .map(deleteSubnet)))
+    .then(subnetIds => callback(null, subnetIds))
+    .catch(callback);
+};
+
+const deleteSubnet = (subnetId: string) => {
+  return ec2.deleteSubnet({
+      SubnetId: subnetId,
+    }).promise();
+};
