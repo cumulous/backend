12a9b92b7d2ef6294dc53092abfbc062f2ac1d3f
Denis Loginov <dloginov@mit.edu>
2017-03-16 21:23:36 +0000

    Fix S3 permissions and variables for BuildProject

--- scripts/swagger-ui.sh
+++ scripts/swagger-ui.sh
@@ -4,7 +4,7 @@ set -e
 
 SWAGGER_API="${PWD}/api/swagger/swagger.yaml"
 SWAGGER_DIST="${PWD}/node_modules/swagger-ui/dist"
-S3_PATH="s3://${ARTIFACT_BUCKET}/api/beta/"
+S3_PATH="s3://${ARTIFACTS_BUCKET}/api/${API_STAGE}/"
 
 cp "${SWAGGER_API}" "${SWAGGER_DIST}"
 cd "${SWAGGER_DIST}"
