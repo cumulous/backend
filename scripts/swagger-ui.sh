168b369f2014f08d7348cd5731eb70164cd151d3
Denis Loginov <dloginov@mit.edu>
2017-03-16 21:15:45 +0000

    Create Swagger-UI distribution and publish API definition to S3

---
+++ scripts/swagger-ui.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+SWAGGER_API="${PWD}/api/swagger/swagger.yaml"
+SWAGGER_DIST="${PWD}/node_modules/swagger-ui/dist"
+S3_PATH="s3://${ARTIFACT_BUCKET}/api/beta/"
+
+cp "${SWAGGER_API}" "${SWAGGER_DIST}"
+cd "${SWAGGER_DIST}"
+rm -f swagger-ui.js
+sed -i 's/swagger-ui\.js/swagger-ui.min.js/' index.html
+aws s3 sync . "${S3_PATH}"
