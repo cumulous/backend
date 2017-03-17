1918f793f1275900c357ac200479732d7a5c35d0
Denis Loginov <dloginov@mit.edu>
2017-03-17 19:50:47 +0000

    Cosmetic fix to BuildProject

--- scripts/swagger-ui.sh
+++ scripts/swagger-ui.sh
@@ -4,7 +4,7 @@ set -e
 
 SWAGGER_API="${PWD}/api/swagger/swagger.yaml"
 SWAGGER_DIST="${PWD}/node_modules/swagger-ui/dist"
-S3_PATH="s3://${ARTIFACTS_BUCKET}/api/${API_STAGE}/"
+S3_PATH="s3://${ARTIFACTS_BUCKET}/api/${BACKEND_STAGE}/"
 
 cp "${SWAGGER_API}" "${SWAGGER_DIST}"
 cd "${SWAGGER_DIST}"
