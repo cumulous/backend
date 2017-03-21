2513fa7fb4cd521aefca69cb41d06734dcbd8ad2
Denis Loginov <dloginov@mit.edu>
2017-03-21 21:25:06 +0000

    Remove all 's in package.sh
    It turns out that AWS CLI doesn't like it when we
    provide names in single quotes.

--- scripts/package.sh
+++ scripts/package.sh
@@ -18,25 +18,25 @@ create_change_set() {
 
   local template_url="s3://${ARTIFACTS_BUCKET}/${stack_name}/sam.yaml"
   local parameters=" \
-    --stack-name '${stack_name}' \
-    --template-url '${template_url}' \
+    --stack-name ${stack_name} \
+    --template-url ${template_url} \
     --change-set-name Deploy \
     --parameters \
-      ParameterKey=VpcRange,ParameterValue='${VPC_RANGE}' \
-      ParameterKey=EncryptionKeyId,ParameterValue='${ENCRYPTION_KEY_ID}' \
-      ParameterKey=SecretsBucket,ParameterValue='${SECRETS_BUCKET}' \
-      ParameterKey=SSHKeyName,ParameterValue='${stack_name}' \
-      ParameterKey=SSHKeyS3Path,ParameterValue='ssh/${stack_name}.pem' \
-      ParameterKey=SSHKeyPutRole,ParameterValue='${SSH_KEY_PUT_ROLE}' \
-      ParameterKey=SSHKeyGetRole,ParameterValue='${SSH_KEY_GET_ROLE}' \
-      ParameterKey=DomainZone,ParameterValue='${DOMAIN_ZONE}' \
-      ParameterKey=APIDomain,ParameterValue='${api_domain}' \
-      ParameterKey=WebDomain,ParameterValue='${web_domain}' \
-      ParameterKey=WebFirewallRanges,ParameterValue='${WEB_FIREWALL_RANGES}' \
-      ParameterKey=Auth0Domain,ParameterValue='${AUTH0_DOMAIN}' \
-      ParameterKey=Auth0CloudFormationClientID,ParameterValue='${AUTH0_CLIENT_ID}' \
-      ParameterKey=Auth0CloudFormationClientSecret,ParameterValue='${AUTH0_CLIENT_SECRET}' \
-      ParameterKey=Auth0CloudFormationSecretsRole,ParameterValue='${AUTH0_SECRETS_ROLE}' "
+      ParameterKey=VpcRange,ParameterValue=${VPC_RANGE} \
+      ParameterKey=EncryptionKeyId,ParameterValue=${ENCRYPTION_KEY_ID} \
+      ParameterKey=SecretsBucket,ParameterValue=${SECRETS_BUCKET} \
+      ParameterKey=SSHKeyName,ParameterValue=${stack_name} \
+      ParameterKey=SSHKeyS3Path,ParameterValue=ssh/${stack_name}.pem \
+      ParameterKey=SSHKeyPutRole,ParameterValue=${SSH_KEY_PUT_ROLE} \
+      ParameterKey=SSHKeyGetRole,ParameterValue=${SSH_KEY_GET_ROLE} \
+      ParameterKey=DomainZone,ParameterValue=${DOMAIN_ZONE} \
+      ParameterKey=APIDomain,ParameterValue=${api_domain} \
+      ParameterKey=WebDomain,ParameterValue=${web_domain} \
+      ParameterKey=WebFirewallRanges,ParameterValue=${WEB_FIREWALL_RANGES} \
+      ParameterKey=Auth0Domain,ParameterValue=${AUTH0_DOMAIN} \
+      ParameterKey=Auth0CloudFormationClientID,ParameterValue=${AUTH0_CLIENT_ID} \
+      ParameterKey=Auth0CloudFormationClientSecret,ParameterValue=${AUTH0_CLIENT_SECRET} \
+      ParameterKey=Auth0CloudFormationSecretsRole,ParameterValue=${AUTH0_SECRETS_ROLE} "
 
   aws s3 cp "${OUTPUT_TEMPLATE}" "${template_url}"
   aws cloudformation create-change-set --change-set-type CREATE ${parameters} ||
