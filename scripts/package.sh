a811dcb0900256d269b9421e0710846ba829b1c4
Denis Loginov <dloginov@mit.edu>
2017-03-23 05:02:17 +0000

    Set backend deployment role in package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -22,6 +22,7 @@ create_change_set() {
   local parameters=" \
     --stack-name ${stack_name} \
     --template-url https://${ARTIFACTS_BUCKET}.s3.amazonaws.com/${stack_name}/${BACKEND_TEMPLATE} \
+    --role-arn ${DEPLOYMENT_ROLE} \
     --change-set-name Deploy \
     --capabilities CAPABILITY_IAM \
     --parameters \
