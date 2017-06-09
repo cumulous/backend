ffca58ecc868a5f371c4189d0840719ef7b6375c
Denis Loginov <dloginov@mit.edu>
2017-06-09 03:10:34 +0000

    Fix parsing of SetupSearchDomain.Fields
    We now pass the entire Fields array as a string,
    and parse it accordingly.

--- src/search.ts
+++ src/search.ts
@@ -6,12 +6,13 @@ import { Callback } from './types';
 export const defineIndexFields = (event: {
       SearchDomain: string;
       FieldSuffix: string;
-      Fields: string[];
+      Fields: string;
     }, context: any, callback: Callback) => {
 
   Promise.resolve(event)
-    .then(event => Promise.all(event.Fields.map(field =>
-      defineIndexField(event.SearchDomain, JSON.parse(field), event.FieldSuffix))))
+    .then(event => JSON.parse(event.Fields))
+    .then(fields => Promise.all(fields.map((field: IndexField) =>
+      defineIndexField(event.SearchDomain, field, event.FieldSuffix))))
     .then(states => callback(null,
       states.some(state => state == 'RequiresIndexDocuments') ?
         'RequiresIndexDocuments' : 'Processing'))
