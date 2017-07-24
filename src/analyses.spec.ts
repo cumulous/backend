68ce2d1c79aade919e8aa4ea8cc277ab905278cc
Denis Loginov <dloginov@mit.edu>
2017-07-24 19:00:51 +0000

    Create and implement POST /analyses/{analysis_id}/execution
    We define a method to submit execution of an analysis.
    We also provide implementation that starts execution of a state
    machine in charge of the actual submission, and records
    it in the database.

--- src/analyses.spec.ts
+++ src/analyses.spec.ts
@@ -3,16 +3,17 @@ import * as uuid from 'uuid';
 
 import * as apig from './apig';
 import { ajv, ApiError } from './apig';
-import { dynamodb } from './aws';
+import { dynamodb, stepFunctions } from './aws';
 import { envNames } from './env';
-import { create } from './analyses';
+import { create, submitExecution } from './analyses';
 import { fakeReject, fakeResolve } from './fixtures/support';
-import { Callback } from './types';
+import { Callback, Dict } from './types';
+
+const fakeAnalysisId = uuid();
+const fakeAnalysesTable = 'fake-analyses-table';
 
 describe('analyses.create()', () => {
   const fakeAnalysisDescription = 'Fake analysis';
-  const fakeAnalysesTable = 'fake-analyses-table';
-  const fakeAnalysisId = uuid();
   const fakePrincipalId = uuid();
   const fakeDate = new Date().toISOString();
 
@@ -117,6 +118,169 @@ describe('analyses.create()', () => {
     it('dynamodb.put() responds with an error', (done: Callback) => {
       err = Error('dynamodb.put()');
       spyOnDynamoDbPut.and.returnValue(fakeReject(err));
+      testError(() => {
+        expect(spyOnRespond).not.toHaveBeenCalled();
+      }, done);
+    });
+
+    it('apig.respond() throws an error', (done: Callback) => {
+      err = Error('apig.respond()');
+      spyOnRespond.and.throwError(err.message);
+      testError(() => {}, done);
+    });
+  });
+});
+
+describe('analyses.submitExecution()', () => {
+  const fakePipelineId = uuid();
+  const fakeDatasetId1 = uuid();
+  const fakeDatasetId2 = uuid();
+  const fakeExecutionStateMachine = 'arn:aws:states:::execution:FakeAnalysisStateMachine';
+
+  const fakeDatasets = (): Dict<string> => ({
+    'Dataset_1': fakeDatasetId1,
+    'Dataset_2': fakeDatasetId2,
+  });
+
+  const fakeExecutionRequest = () => ({
+    pipeline_id: fakePipelineId,
+    datasets: fakeDatasets(),
+  });
+
+  const fakeRequest = (validated = true) => ({
+    body: validated ? fakeExecutionRequest() : stringify(fakeExecutionRequest()),
+    pathParameters: {
+      analysis_id: fakeAnalysisId,
+    },
+  });
+
+  const fakeExecution = () => ({
+    analysis_id: fakeAnalysisId,
+    pipeline_id: fakePipelineId,
+    datasets: fakeDatasets(),
+  });
+
+  const testMethod = (callback: Callback) =>
+    submitExecution(fakeRequest(false), null, callback);
+
+  let spyOnValidate: jasmine.Spy;
+  let spyOnDynamoDbUpdate: jasmine.Spy;
+  let spyOnStartExecution: jasmine.Spy;
+  let spyOnRespond: jasmine.Spy;
+  let spyOnRespondWithError: jasmine.Spy;
+
+  beforeEach(() => {
+    process.env[envNames.analysesTable] = fakeAnalysesTable;
+    process.env[envNames.stateMachine] = fakeExecutionStateMachine;
+
+    spyOnValidate = spyOn(apig, 'validate')
+      .and.callThrough();
+    spyOnDynamoDbUpdate = spyOn(dynamodb, 'update')
+      .and.returnValue(fakeResolve());
+    spyOnStartExecution = spyOn(stepFunctions, 'startExecution')
+      .and.returnValue(fakeResolve());
+    spyOnRespond = spyOn(apig, 'respond')
+      .and.callFake((callback: Callback) => callback());
+    spyOnRespondWithError = spyOn(apig, 'respondWithError')
+      .and.callFake((callback: Callback) => callback());
+  });
+
+  it('calls apig.validate() once with correct parameters', (done: Callback) => {
+    testMethod(() => {
+      expect(spyOnValidate).toHaveBeenCalledWith(fakeRequest(), 'POST', '/analyses/{analysis_id}/execution');
+      expect(spyOnValidate).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it('calls dynamodb.update() once with correct parameters', (done: Callback) => {
+    testMethod(() => {
+      expect(spyOnDynamoDbUpdate).toHaveBeenCalledWith({
+        TableName: fakeAnalysesTable,
+        Key: {
+          id: fakeAnalysisId,
+        },
+        UpdateExpression: 'set #s = :sub',
+        ConditionExpression: '(#s = :c) or (#s = :f) or (#s = :suc)',
+        ExpressionAttributeNames: {
+          '#s': 'status',
+        },
+        ExpressionAttributeValues: {
+          ':c': 'created',
+          ':sub': 'submitted',
+          ':f': 'failed',
+          ':suc': 'succeeded',
+        },
+      });
+      expect(spyOnDynamoDbUpdate).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it('calls stepFunctions.startExecution() once with correct parameters', (done: Callback) => {
+    testMethod(() => {
+      expect(spyOnStartExecution).toHaveBeenCalledWith({
+        stateMachineArn: fakeExecutionStateMachine,
+        input: stringify(fakeExecution()),
+      });
+      expect(spyOnStartExecution).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it('calls apig.respond() once with correct parameters', (done: Callback) => {
+    const callback = () => {
+      expect(spyOnRespond).toHaveBeenCalledWith(callback, fakeRequest(), fakeExecution());
+      expect(ajv.validate('spec#/definitions/AnalysisExecution', fakeExecution())).toBe(true);
+      expect(spyOnRespond).toHaveBeenCalledTimes(1);
+      done();
+    };
+    testMethod(callback);
+  });
+
+  describe('calls apig.respondWithError() immediately with the error if', () => {
+    let err: Error | ApiError | jasmine.ObjectContaining<{ code: number }>;
+
+    const testError = (after: Callback, done: Callback, validated = true) => {
+      const callback = () => {
+        expect(spyOnRespondWithError).toHaveBeenCalledWith(
+          callback, fakeRequest(validated), err);
+        expect(spyOnRespondWithError).toHaveBeenCalledTimes(1);
+        after();
+        done();
+      };
+      testMethod(callback);
+    };
+
+    it('apig.validate() responds with an error', (done: Callback) => {
+      err = new ApiError('validate()');
+      spyOnValidate.and.returnValue(Promise.reject(err));
+      testError(() => {
+        expect(spyOnDynamoDbUpdate).not.toHaveBeenCalled();
+      }, done, false);
+    });
+
+    it('dynamodb.update() responds with an error', (done: Callback) => {
+      err = Error('dynamodb.update()');
+      spyOnDynamoDbUpdate.and.returnValue(fakeReject(err));
+      testError(() => {
+        expect(spyOnStartExecution).not.toHaveBeenCalled();
+      }, done);
+    });
+
+    it('dynamodb.update() responds with ConditionalCheckFailedException', (done: Callback) => {
+      err = jasmine.objectContaining({ code: 409 });
+      const errUpdate = new ApiError('dynamodb.update()',
+        undefined, 'ConditionalCheckFailedException');
+      spyOnDynamoDbUpdate.and.returnValue(fakeReject(errUpdate));
+      testError(() => {
+        expect(spyOnStartExecution).not.toHaveBeenCalled();
+      }, done);
+    });
+
+    it('stepFunctions.startExecution() responds with an error', (done: Callback) => {
+      err = Error('stepFunctions.startExecution()');
+      spyOnStartExecution.and.returnValue(fakeReject(err));
       testError(() => {
         expect(spyOnRespond).not.toHaveBeenCalled();
       }, done);
--- src/analyses.ts
+++ src/analyses.ts
@@ -1,9 +1,10 @@
+import * as stringify from 'json-stable-stringify';
 import { v4 as uuid } from 'uuid';
 
-import { Request, respond, respondWithError, validate } from './apig';
-import { dynamodb } from './aws';
+import { ApiError, Request, respond, respondWithError, validate } from './apig';
+import { dynamodb, stepFunctions } from './aws';
 import { envNames } from './env';
-import { Callback } from './types';
+import { Callback, Dict } from './types';
 
 interface AnalysisCreationRequest {
   description: string;
@@ -28,3 +29,57 @@ const generateAnalysis = (request: AnalysisCreationRequest, principalId: string)
   created_by: principalId,
   status: 'created',
 });
+
+export const submitExecution = (request: Request, context: any, callback: Callback) => {
+  validate(request, 'POST', '/analyses/{analysis_id}/execution')
+    .then(() => setExecutionSubmittedStatus(request.pathParameters.analysis_id))
+    .then(() => startExecution(request.pathParameters.analysis_id, request.body))
+    .then(execution => respond(callback, request, execution))
+    .catch(err => respondWithError(callback, request, err));
+};
+
+const setExecutionSubmittedStatus = (id: string) => {
+  return dynamodb.update({
+    TableName: process.env[envNames.analysesTable],
+    Key: {
+      id,
+    },
+    UpdateExpression: 'set #s = :sub',
+    ConditionExpression: '(#s = :c) or (#s = :f) or (#s = :suc)',
+    ExpressionAttributeNames: {
+      '#s': 'status',
+    },
+    ExpressionAttributeValues: {
+      ':c': 'created',
+      ':sub': 'submitted',
+      ':f': 'failed',
+      ':suc': 'succeeded',
+    },
+  }).promise()
+    .catch(err => {
+      if (err.code === 'ConditionalCheckFailedException') {
+        err = new ApiError('Conflict', ["Analysis must be in 'created', 'failed', or 'succeeded' " +
+          "state before it can be (re-)run"], 409);
+      }
+      throw err;
+    });
+};
+
+interface ExecutionParameters {
+  pipeline_id: string;
+  datasets: Dict<string>;
+}
+
+const startExecution = (analysis_id: string, params: ExecutionParameters) => {
+  return Promise.resolve()
+    .then(() => ({
+      analysis_id,
+      pipeline_id: params.pipeline_id,
+      datasets: params.datasets,
+    }))
+    .then(execution => stepFunctions.startExecution({
+      stateMachineArn: process.env[envNames.stateMachine],
+      input: stringify(execution),
+    }).promise()
+      .then(() => execution));
+};
