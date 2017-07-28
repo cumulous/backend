31524149db366ecf8ac2369ab10c205fb339c600
Denis Loginov <dloginov@mit.edu>
2017-07-28 02:42:54 +0000

    Set proper permissions for mounted directory in init.sh
    We change the owner of the mounted path to UID 100.
    We also add some minor cosmetic fixes to init.sh

--- src/init.sh
+++ src/init.sh
@@ -3,12 +3,14 @@
 DEVICES="$1"
 MOUNT="$2"
 
+UID=100
+
 set -e
 
 d=0
 for device in ${DEVICES}; do
   d=$((d + 1))
-  while [ ! -b $device ]; do
+  while [ ! -b ${device} ]; do
     sleep 1
   done
 done
@@ -18,13 +20,14 @@ if [ $d = 1 ]; then
 else
   drive=/dev/md0
   yum install -y mdadm
-  mdadm --create $drive --level 0 --raid-devices $d ${DEVICES}
+  mdadm --create ${drive} --level 0 --raid-devices $d ${DEVICES}
 fi
 
-mkfs.ext4 $drive
+mkfs.ext4 ${drive}
 mkdir -p ${MOUNT}
-mount $drive ${MOUNT}
-chmod o+w ${MOUNT}
+mount ${drive} ${MOUNT}
+chown ${UID}:root ${MOUNT}
+chmod 660 ${MOUNT}
 
 service docker restart || true
 start ecs || true
